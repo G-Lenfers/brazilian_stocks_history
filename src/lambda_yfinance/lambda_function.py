4e262c5642343c2710534add2c862d70f0b7f29a
Guilherme Lenfers <guilherme.dornelles@gmail.com>
2023-01-15 14:47:46 -0300

    Wrote yfinance methods to get stocks historical information

--- src/lambda_yfinance/lambda_function.py
+++ src/lambda_yfinance/lambda_function.py
@@ -1,16 +1,19 @@
 """File for getting brazilian stocks through yfinance package."""
+import pandas as pd
 import yfinance as yf
+from sqlalchemy import create_engine
 
 
 def lambda_handler(event: any) -> None:
     """Main function."""
-    stocks = event.get("stocks")
+    selected_columns = ['Date', 'Open', 'Close', 'High', 'Low', 'Volume']
 
+    stocks = event.get("stocks")
     for stock in stocks:
 
-        ticker = yf.Ticker(stock)
-        # The oldest value is the first day of 2000
-        print(ticker.history(period="max"))
+        history_dataframe = yf.download(tickers=stock, period='max', ignore_tz=True)
+        history_dataframe.reset_index(inplace=True)
+        history_dataframe = history_dataframe[selected_columns]
 
 
 if __name__ == "__main__":
