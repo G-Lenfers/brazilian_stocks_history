0b18b9c86ccf024ffea33d2a3fd44585760782a1
Guilherme Lenfers <guilherme.dornelles@gmail.com>
2023-01-15 15:14:55 -0300

    Refactored transform method.

--- src/lambda_yfinance/lambda_function.py
+++ src/lambda_yfinance/lambda_function.py
@@ -6,21 +6,34 @@ from sqlalchemy import create_engine
 
 def lambda_handler(event: any) -> None:
     """Main function."""
-    selected_columns = ['Date', 'Open', 'Close', 'High', 'Low', 'Volume']
-
     stocks = event.get("stocks")
     for stock in stocks:
 
         # Extract
         extracted_history = _download_ticker_information(stock=stock)
-        history_dataframe.reset_index(inplace=True)
-        history_dataframe = history_dataframe[selected_columns]
+
+        # Transform
+        history_dataframe = _transform_dataframe(dataframe=extracted_history)
 
 
 def _download_ticker_information(stock):
     """Get ticker information from yahoo API."""
     return yf.download(tickers=stock, period='max', ignore_tz=True)
 
+
+def _transform_dataframe(dataframe):
+    """Insert docstring here."""
+    # Date index to column
+    dataframe.reset_index(inplace=True)
+
+    # Order columns
+    selected_columns = ['Date', 'Open', 'Close', 'High', 'Low', 'Volume']
+    dataframe = dataframe[selected_columns]
+
+    # Rename columns (postgres treat uppercase as a special character)
+    dataframe.rename(str.lower, axis='columns', inplace=True)
+
+    return dataframe
 
 if __name__ == "__main__":
     event = {
