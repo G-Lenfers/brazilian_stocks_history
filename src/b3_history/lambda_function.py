36a9fc1a1f440c461d04ac7a9fb9730b09f98e93
Guilherme Lenfers <guilherme.dornelles@gmail.com>
2023-02-05 14:13:42 -0300

    Refactored the code

--- src/b3_history/lambda_function.py
+++ src/b3_history/lambda_function.py
@@ -13,7 +13,7 @@ ROOT_PATH = os.path.abspath(  # return the absolute path of the following
 RESOURCES_PATH = '/resources/'
 
 
-def open_zipped_file(file_name: str):
+def _open_zipped_file(file_name: str):
     """Open zipped file and read it in non-binary mode."""
     zipped_file = ROOT_PATH + RESOURCES_PATH + file_name
     with zipfile.ZipFile(zipped_file, 'r') as my_zip:
@@ -24,8 +24,7 @@ def open_zipped_file(file_name: str):
         )
 
 
-with open_zipped_file(file_name='COTAHIST_A1986.zip') as file:
-    slice_collection = {
+slice_collection = {
         'tipo_de_registro': slice(0, 2),
         'data_pregao': slice(2, 10),
         'codigo_bdi': slice(10, 12),
@@ -53,63 +52,75 @@ with open_zipped_file(file_name='COTAHIST_A1986.zip') as file:
         'codigo_papel_isin': slice(230, 242),
         'numero_distribuicao_papel': slice(242, 245)
     }
-    dataframe = pd.DataFrame()
 
-    # Have a look into the following idea from stackoverflow
-    # with open("file") as fp:
-    #     for i, line in enumerate(fp):
-    # Com isso não preciso definir na mão o loop_counter
 
-    loop_counter = 0
-    for text_line in file:
+def read_and_extract_data_from_file(file_name: str, last_iteration=0) -> dict:
+    """Unzip, read, and store data into pandas dataframe."""
+    with _open_zipped_file(file_name=file_name) as file:
+        dataframe = pd.DataFrame()
+        for i, text_line in enumerate(file):
 
-        # ignore first line and TODO last line
-        if loop_counter == 0:
-            loop_counter += 1
-            continue
+            if i < last_iteration:
+                continue
 
-        my_dict = {
-            'tipo_de_registro': text_line[slice_collection['tipo_de_registro']],
-            'data_pregao': text_line[slice_collection['data_pregao']],
-            'codigo_bdi': text_line[slice_collection['codigo_bdi']],
-            'codigo_negociaco_papel': text_line[slice_collection['codigo_negociaco_papel']],
-            'tipo_de_mercado': text_line[slice_collection['tipo_de_mercado']],
-            'nome_resumido': text_line[slice_collection['nome_resumido']],
-            'especificacao_papel': text_line[slice_collection['especificacao_papel']],
-            'prazo_dias_mercado_termo': text_line[slice_collection['prazo_dias_mercado_termo']],
-            'moeda_referencia': text_line[slice_collection['moeda_referencia']],
-            'preco_abertura_pregao': text_line[slice_collection['preco_abertura_pregao']],
-            'preco_maximo_pregao': text_line[slice_collection['preco_maximo_pregao']],
-            'preco_minimo_pregao': text_line[slice_collection['preco_minimo_pregao']],
-            'preco_medio_pregao': text_line[slice_collection['preco_medio_pregao']],
-            'preco_ultimo_negocio': text_line[slice_collection['preco_ultimo_negocio']],
-            'preco_melhor_oferta_compra': text_line[slice_collection['preco_melhor_oferta_compra']],
-            'preco_melhor_oferta_venda': text_line[slice_collection['preco_melhor_oferta_venda']],
-            'numero_negocios_efetuados': text_line[slice_collection['numero_negocios_efetuados']],
-            'quantidade_total_titulos_negociados': text_line[slice_collection['quantidade_total_titulos_negociados']],
-            'preco_exercicio_opcoes': text_line[slice_collection['preco_exercicio_opcoes']],
-            'indicador_correcao_precos': text_line[slice_collection['indicador_correcao_precos']],
-            'data_vencimento_opcoes': text_line[slice_collection['data_vencimento_opcoes']],
-            'fator_cotacao_papel': text_line[slice_collection['fator_cotacao_papel']],
-            'preco_exercicio_pontos_opcoes': text_line[slice_collection['preco_exercicio_pontos_opcoes']],
-            'codigo_papel_isin': text_line[slice_collection['codigo_papel_isin']],
-            'numero_distribuicao_papel': text_line[slice_collection['numero_distribuicao_papel']]
+            sliced_text_line = _slice_text_data(text_line=text_line)
+
+            dataframe = pd.concat(
+                [
+                    dataframe,
+                    pd.DataFrame(
+                        sliced_text_line,
+                        index=[0]
+                    )
+                ],
+                ignore_index=True
+            )
+
+            if i != 0 and i % 10000 == 0:
+                print("Dataframe cap of 10000 lines reached! Returning...")
+                return {
+                    "has_more": True,
+                    "extracted_data": dataframe
+                }
+
+        print(f"Reached the end of file {file_name}.")
+        return {
+            "has_more": False,
+            "extracted_data": dataframe
         }
 
-        dataframe = pd.concat(
-            [
-                dataframe,
-                pd.DataFrame(
-                    my_dict,
-                    index=[0]
-                )
-            ],
-            ignore_index=True
-        )
 
-        loop_counter += 1
-        if loop_counter % 1000 == 0:
-            print("lines_read:", loop_counter)
+def _slice_text_data(text_line):
+    return {
+        'tipo_de_registro': text_line[slice_collection['tipo_de_registro']],
+        'data_pregao': text_line[slice_collection['data_pregao']],
+        'codigo_bdi': text_line[slice_collection['codigo_bdi']],
+        'codigo_negociaco_papel': text_line[slice_collection['codigo_negociaco_papel']],
+        'tipo_de_mercado': text_line[slice_collection['tipo_de_mercado']],
+        'nome_resumido': text_line[slice_collection['nome_resumido']],
+        'especificacao_papel': text_line[slice_collection['especificacao_papel']],
+        'prazo_dias_mercado_termo': text_line[slice_collection['prazo_dias_mercado_termo']],
+        'moeda_referencia': text_line[slice_collection['moeda_referencia']],
+        'preco_abertura_pregao': text_line[slice_collection['preco_abertura_pregao']],
+        'preco_maximo_pregao': text_line[slice_collection['preco_maximo_pregao']],
+        'preco_minimo_pregao': text_line[slice_collection['preco_minimo_pregao']],
+        'preco_medio_pregao': text_line[slice_collection['preco_medio_pregao']],
+        'preco_ultimo_negocio': text_line[slice_collection['preco_ultimo_negocio']],
+        'preco_melhor_oferta_compra': text_line[slice_collection['preco_melhor_oferta_compra']],
+        'preco_melhor_oferta_venda': text_line[slice_collection['preco_melhor_oferta_venda']],
+        'numero_negocios_efetuados': text_line[slice_collection['numero_negocios_efetuados']],
+        'quantidade_total_titulos_negociados': text_line[slice_collection['quantidade_total_titulos_negociados']],
+        'preco_exercicio_opcoes': text_line[slice_collection['preco_exercicio_opcoes']],
+        'indicador_correcao_precos': text_line[slice_collection['indicador_correcao_precos']],
+        'data_vencimento_opcoes': text_line[slice_collection['data_vencimento_opcoes']],
+        'fator_cotacao_papel': text_line[slice_collection['fator_cotacao_papel']],
+        'preco_exercicio_pontos_opcoes': text_line[slice_collection['preco_exercicio_pontos_opcoes']],
+        'codigo_papel_isin': text_line[slice_collection['codigo_papel_isin']],
+        'numero_distribuicao_papel': text_line[slice_collection['numero_distribuicao_papel']]
+    }
 
 
-print(dataframe)
+if __name__ == "__main__":
+    read_and_extract_data_from_file(
+        file_name='COTAHIST_A1986.zip'
+    )
