6c320615b179a68085becec44d32682c1975dab3
Guilherme Lenfers <guilherme.dornelles@gmail.com>
2023-04-05 18:43:31 -0300

    Created data warehouse app file

---
+++ src/data_warehouse/app.py
@@ -0,0 +1,14 @@
+"""Filter specific ticket data and upload it to data warehouse."""
+
+
+if __name__ == "__main__":
+    event = [
+        {
+            "ticket_name": "VALE3",
+            "optional_old_ticket_name": "VAL 3"
+        },
+        {
+            "ticket_name": "PETR3",
+            "optional_old_ticket_name": "PET 3"
+        }
+    ]
