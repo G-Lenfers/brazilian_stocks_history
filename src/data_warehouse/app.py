6dbfca2ddae191b9245c9b50c60bff47a51aee7e
Guilherme Lenfers <guilherme.dornelles@gmail.com>
2023-04-07 07:36:11 -0300

    Refactored extraction method

--- src/data_warehouse/app.py
+++ src/data_warehouse/app.py
@@ -12,36 +12,28 @@ class DataWarehouseMainEngine:
         self.schema = "data_warehouse"
         self.postgres = PostgresConnector(schema=self.schema)
 
-
-def lambda_function(event: list) -> None:
-    """Orchestrate accordingly."""
-    # Instance main engine
-    engine = DataWarehouseMainEngine()
-
-    engine.postgres.create_schema_database()  # must have 'create' privilege
-
-    # Extract
-    # TODO Check view existence
-    # TODO schema name as parameter from event
-    data_lake_extraction_query = """
-        SELECT
-            sh.data_pregao,
-            sh.codigo_negociaco_papel,
-            sh.nome_resumido,
-            sh.moeda_referencia,
-            sh.preco_abertura_pregao,
-            sh.preco_ultimo_negocio,
-            sh.preco_maximo_pregao,
-            sh.preco_minimo_pregao
-        FROM b3_history.stocks_history sh 
-        WHERE sh.tipo_de_mercado = '010'
-        -- ORDER BY data_pregao ASC
-    """  # TODO order by in pandas, not in query
-    for stock in event:
+    def extract_data_lake(self, stock: dict) -> pd.DataFrame:
+        """Get ticket data from Data Lake."""
+        # TODO Check view existence
+        # TODO schema name as parameter from event
+        data_lake_extraction_query = """
+            SELECT
+                sh.data_pregao,
+                sh.codigo_negociaco_papel,
+                sh.nome_resumido,
+                sh.moeda_referencia,
+                sh.preco_abertura_pregao,
+                sh.preco_ultimo_negocio,
+                sh.preco_maximo_pregao,
+                sh.preco_minimo_pregao
+            FROM b3_history.stocks_history sh 
+            WHERE sh.tipo_de_mercado = '010'
+            -- ORDER BY data_pregao ASC
+        """  # TODO order by in pandas, not in query
 
         if not stock.get('ticket_name'):
             print('Main ticket name is mandatory. Skipping...')
-            continue
+            return pd.DataFrame()
 
         if stock.get('optional_old_ticket_name'):
             query_conditional = """
@@ -61,11 +53,26 @@ def lambda_function(event: list) -> None:
             query_parameters = {
                 "ticket_name": stock.get('ticket_name'),
             }
-        extracted_ticket_data = engine.postgres.read_sql_query(
+
+        extracted_ticket_data = self.postgres.read_sql_query(
             query=data_lake_extraction_query+query_conditional,
             params=query_parameters
         )
+        return extracted_ticket_data
 
+
+
+def lambda_function(event: list) -> None:
+    """Orchestrate accordingly."""
+    # Instance main engine
+    engine = DataWarehouseMainEngine()
+
+    engine.postgres.create_schema_database()  # must have 'create' privilege
+
+    for stock in event:
+
+        # Extract
+        extracted_ticket_data = engine.extract_data_lake(stock=stock)
 
 
 if __name__ == "__main__":
