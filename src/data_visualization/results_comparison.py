f83ace6c53cf1fe10c3af4fae6ccb01e439e51f2
Guilherme Lenfers <guilherme.dornelles@gmail.com>
2023-04-15 15:26:22 -0300

    Plot another results figure

--- src/data_visualization/results_comparison.py
+++ src/data_visualization/results_comparison.py
@@ -13,12 +13,8 @@ import matplotlib.pyplot as plt
 
 from src.shared.loading_engine import PostgresConnector
 
-# TODO label size
-# TODO filtered plot, with all 4 variables
 # TODO plot comparison
 
-# # y_query = "SELECT * FROM yahoo_finance.petr4_sa"
-# # y_dataframe = postgres.read_sql_query(query=y_query, params={})
 ROOT_PATH = os.path.abspath(os.path.join(__file__, os.pardir))
 
 
@@ -29,7 +25,7 @@ def lambda_handler():
     dw_query = "SELECT * FROM data_warehouse.petr3"
     dw_dataframe = postgres.read_sql_query(query=dw_query, params={})
 
-    build_figure_dw_all_dates(dataframe=dw_dataframe)
+    # build_figure_dw_all_dates(dataframe=dw_dataframe)
     build_figure_filtered_dates(dataframe=dw_dataframe)
 
 
@@ -56,9 +52,32 @@ def build_figure_dw_all_dates(dataframe) -> None:
 
 
 def build_figure_filtered_dates(dataframe) -> None:
-    """"""
-    date_filter = dataframe['data_pregao'] >= date(1994, 1, 1)
-    pass
+    """Plot results after July the first, 1994."""
+    date_filter = dataframe['data_pregao'] >= date(1994, 7, 1)
+    x = dataframe.loc[date_filter, 'data_pregao']
+    y1 = dataframe.loc[date_filter, 'preco_abertura_pregao']
+    y2 = dataframe.loc[date_filter, 'preco_ultimo_negocio']
+    y3 = dataframe.loc[date_filter, 'preco_maximo_pregao']
+    y4 = dataframe.loc[date_filter, 'preco_minimo_pregao']
+
+    fig, ax = plt.subplots(1, 1, figsize=(6, 7))
+    ax.plot(x, y1, linewidth=2.0, label="Preço abertura pregão")
+    ax.plot(x, y2, linewidth=2.0, label="Preço encerramento pregão")
+    ax.plot(x, y3, linewidth=2.0, label="Preço máximo pregão")
+    ax.plot(x, y4, linewidth=2.0, label="Preço mínimo pregão")
+
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%b-%Y'))
+    for label in ax.get_xticklabels(which='major'):
+        label.set(rotation=30)
+
+    plt.grid()
+    plt.legend()
+    plt.xlabel("Data do pregão")
+    plt.ylabel("Preço do ativo (R\$)")
+    plt.subplots_adjust(left=0.15, bottom=0.15)
+    plt.tight_layout()
+    # plt.show()
+    plt.savefig(ROOT_PATH + '/figures/filtered_dates.pdf')
 
 
 if __name__ == "__main__":
